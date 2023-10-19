f6e2b72e9d31f7d278f8fdfcdec85391047744d6
Himanshu Dixit <hudixt@gmail.com>
2023-10-19 19:08:40 +0000

    feat: update API

--- pages/api/v1/user-broker/swastika/membership.js
+++ pages/api/v1/user-broker/swastika/membership.js
@@ -36,11 +36,204 @@ const ELIGIBLE_SAMPLE =  {
 	}
 }
 
-const stage = {
-	ELIGIBLE:ELIGIBLE_SAMPLE
+
+const NOT_ELIGIBLE_SAMPLE =  {
+	"brokerDetails": {
+	  "name": "Swastika",
+	  "logoUrl": "https://example.com/logo.png"
+	},
+	"brokingRegistrationPlans": [
+	  {
+		"id": "12345",
+		"name": "Default",
+		"registrationPlan": {
+			fees: 300,
+			registrationRequired: true
+		},
+		"brokingPlan": {
+		  "intraday": {
+			"price": 20,
+	
+		  },
+		  "fno": {
+			"price": 20,
+
+		  },
+		  "delivery": {
+			"price": 200,
+		  }
+		},
+		"meta": {
+		  "mintCoinMultiplier": 5
+		}
+	  },
+	],
+	"currentUserState": {
+	  "status": "NOT_ELIGIBLE",
+	  "statusData": {}
+	}
+}
+
+const JOINED_WAITLIST_SAMPLE =  {
+	"brokerDetails": {
+	  "name": "Swastika",
+	  "logoUrl": "https://example.com/logo.png"
+	},
+	"brokingRegistrationPlans": [
+	  {
+		"id": "12345",
+		"name": "Default",
+		"registrationPlan": {
+			fees: 300,
+			registrationRequired: true
+		},
+		"brokingPlan": {
+		  "intraday": {
+			"price": 20,
+	
+		  },
+		  "fno": {
+			"price": 20,
+
+		  },
+		  "delivery": {
+			"price": 200,
+		  }
+		},
+		"meta": {
+		  "mintCoinMultiplier": 5
+		}
+	  },
+	],
+	"currentUserState": {
+	  "status": "JOINED_WAITLIST",
+	  "statusData": {}
+	}
+}
+
+const PAYMENT_INIT =  {
+	"brokerDetails": {
+	  "name": "Swastika",
+	  "logoUrl": "https://example.com/logo.png"
+	},
+	"brokingRegistrationPlans": [
+	  {
+		"id": "12345",
+		"name": "Default",
+		"registrationPlan": {
+			fees: 300,
+			registrationRequired: true
+		},
+		"brokingPlan": {
+		  "intraday": {
+			"price": 20,
+	
+		  },
+		  "fno": {
+			"price": 20,
+
+		  },
+		  "delivery": {
+			"price": 200,
+		  }
+		},
+		"meta": {
+		  "mintCoinMultiplier": 5
+		}
+	  },
+	],
+	"currentUserState": {
+	  "status": "PAYMENT_INIT",
+	  "statusData": {}
+	}
+}
+
+const PAYMENT_FAILED =  {
+	"brokerDetails": {
+	  "name": "Swastika",
+	  "logoUrl": "https://example.com/logo.png"
+	},
+	"brokingRegistrationPlans": [
+	  {
+		"id": "12345",
+		"name": "Default",
+		"registrationPlan": {
+			fees: 300,
+			registrationRequired: true
+		},
+		"brokingPlan": {
+		  "intraday": {
+			"price": 20,
+	
+		  },
+		  "fno": {
+			"price": 20,
+
+		  },
+		  "delivery": {
+			"price": 200,
+		  }
+		},
+		"meta": {
+		  "mintCoinMultiplier": 5
+		}
+	  },
+	],
+	"currentUserState": {
+	  "status": "PAYMENT_FAILED",
+	  "statusData": {}
+	}
+}
+
+const PAYMENT_SUCCESS =  {
+	"brokerDetails": {
+	  "name": "Swastika",
+	  "logoUrl": "https://example.com/logo.png"
+	},
+	"brokingRegistrationPlans": [
+	  {
+		"id": "12345",
+		"name": "Default",
+		"registrationPlan": {
+			fees: 300,
+			registrationRequired: true
+		},
+		"brokingPlan": {
+		  "intraday": {
+			"price": 20,
+	
+		  },
+		  "fno": {
+			"price": 20,
+
+		  },
+		  "delivery": {
+			"price": 200,
+		  }
+		},
+		"meta": {
+		  "mintCoinMultiplier": 5
+		}
+	  },
+	],
+	"currentUserState": {
+	  "status": "PAYMENT_SUCCESS",
+	  "statusData": {}
+	}
+}
+
+const stageValues = {
+	1:NOT_ELIGIBLE_SAMPLE,
+	2:JOINED_WAITLIST_SAMPLE,
+	3:ELIGIBLE_SAMPLE,
+	4:PAYMENT_INIT,
+	5:PAYMENT_FAILED,
+	6:PAYMENT_SUCCESS,
 }
 function handler(req, res) {
-	res.status(200).json(stage.ELIGIBLE);
+	const query = req.query;
+	const {stage} = query
+	res.status(200).json(stageValues[stage]);
 }
 
 export default handler;
